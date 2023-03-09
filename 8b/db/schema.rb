2909e2db716fb55a7bb875344afad58e13348b0a
Egyh <124222570+Egyh@users.noreply.github.com>
2023-03-09 09:34:25 +0000

    グループ機能追加

--- 8b/db/schema.rb
+++ 8b/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_09_022227) do
+ActiveRecord::Schema.define(version: 2023_03_09_075536) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2023_03_09_022227) do
   create_table "group_users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "group_id"
   end
 
   create_table "groups", force: :cascade do |t|
